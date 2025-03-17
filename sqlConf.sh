9d25aba5c7ebbee97edd586095be7768dc04494c
VitorAlmeida01 <vitor.almeida@sptech.school>
2025-03-16 21:59:10 -0300

    teste4

--- sqlConf.sh
+++
@@ -1,8 +0,0 @@
-# Caminho do arquivo
-ARQUIVO="/etc/mysql/mysql.conf.d/mysqld.cnf"
-
-# Faz o replace da linha
-sudo sed -i 's/^bind-address\s*=.*$/bind-address = 0.0.0.0/' "$ARQUIVO"
-
-# Reinicia o MySQL para aplicar
-sudo systemctl restart mysql
